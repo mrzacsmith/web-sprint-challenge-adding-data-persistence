2cbfda2a67f2d3327c8baf11cbc1118c036e7e58
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-18 20:55:12 +0100

    wip

---
+++ data/migrations/20201118204055_projects.js
@@ -0,0 +1,12 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("projects", function(projects) {
+      projects.increments();
+      projects.string("name", 128).notNullable();
+      projects.text("description");
+      projects.boolean("completed").defaultTo(false);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("projects");
+};
