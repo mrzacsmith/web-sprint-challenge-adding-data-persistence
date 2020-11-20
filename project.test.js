7f8d18ce4ad3d9fac71b1a9e4abc12df75b17a68
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-20 10:57:31 +0100

    add tests

--- project.test.js
+++ project.test.js
@@ -7,19 +7,15 @@ const Project = require('./api/project/model')
 const Resource = require('./api/resource/model')
 const Task = require('./api/task/model')
 
-const projectA = { name: 'a', description: 'b' }
-const projectB = { name: 'c', description: 'd', completed: true }
-const projectC = { name: 'e' }
+const projectA = { name: 'Web API', description: 'Build APIs' }
+const projectB = { name: 'Databases', description: 'Learn SQL', completed: true }
+const projectC = { name: 'Authentication' }
+
+const resourceA = { name: 'keyboard' }
+const resourceB = { name: 'computer', description: 'Windows PC' }
 
 beforeAll(async () => {
   await db.migrate.latest()
-})
-beforeEach(async () => {
-  await db('tasks').truncate()
-  await db('resources').truncate()
-  await db('projects').truncate()
-  await db('projects').insert(projectA)
-  await db('projects').insert({ ...projectB, completed: 1 })
 })
 
 it('sanity check', () => {
@@ -28,6 +24,11 @@ it('sanity check', () => {
 
 describe('server.js', () => {
   describe('-------------------- projects endpoints', () => {
+    beforeEach(async () => {
+      await db('projects').truncate()
+      await db('projects').insert(projectA)
+      await db('projects').insert({ ...projectB, completed: 1 })
+    })
     describe('[GET] /api/projects', () => {
       it('can get all projects', async () => {
         const res = await request(server).get('/api/projects')
@@ -51,27 +52,56 @@ describe('server.js', () => {
         expect(projects[1]).toMatchObject({ ...projectB, completed: 1 })
         expect(projects[2]).toMatchObject({ ...projectC, description: null, completed: 0 })
       })
-      it('responds with the newly created resource', async () => {
+      it('responds with the newly created project', async () => {
         await db('projects').truncate()
         const res = await request(server).post('/api/projects').send(projectA)
         expect(res.body).toMatchObject({ ...projectA, completed: false })
       })
-      it('rejects projects lacking a description with a "client error" status code', async () => {
+      it('rejects projects lacking a description with an error status code', async () => {
         await db('projects').truncate()
         const res = await request(server).post('/api/projects').send({})
         const projects = await db('projects')
-        expect(res.status + '').toMatch(/4/)
+        expect(res.status + '').toMatch(/4|5/)
         expect(projects).toHaveLength(0)
       })
     })
   })
 
   describe('-------------------- resources endpoints', () => {
+    beforeEach(async () => {
+      await db('resources').truncate()
+      await db('resources').insert(resourceA)
+      await db('resources').insert(resourceB)
+    })
     describe('[GET] /api/resources', () => {
-
+      it('can get all resources', async () => {
+        const res = await request(server).get('/api/resources')
+        expect(res.body).toHaveLength(2)
+        expect(res.body[0]).toMatchObject(resourceA)
+        expect(res.body[1]).toMatchObject(resourceB)
+      })
     })
     describe('[POST] /api/resources', () => {
-
+      it('can add a new resource to the db', async () => {
+        await db('resources').truncate()
+        await request(server).post('/api/resources').send(resourceA)
+        await request(server).post('/api/resources').send(resourceB)
+        const resources = await db('resources')
+        expect(resources).toHaveLength(2)
+        expect(resources[0]).toMatchObject(resourceA)
+        expect(resources[1]).toMatchObject(resourceB)
+      })
+      it('responds with the newly created resource', async () => {
+        await db('resources').truncate()
+        const res = await request(server).post('/api/resources').send(resourceA)
+        expect(res.body).toMatchObject(resourceA)
+      })
+      it('rejects a resource with an existing name with an error status code', async () => {
+        const res = await request(server).post('/api/resources').send(resourceA)
+        const resources = await db('resources')
+        expect(res.status + '').toMatch(/4|5/)
+        expect(resources).toHaveLength(2)
+      })
     })
   })
 
