52f0349cff0e743121e97f19dcc762a93a1feea7
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-02-18 16:29:41 +0100

    give them db config

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,3 @@
-// Complete your db configuration using the `environment` variable.
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 const environment = process.env.NODE_ENV || 'development';
